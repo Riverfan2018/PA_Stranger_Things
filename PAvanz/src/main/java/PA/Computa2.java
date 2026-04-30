f8da47ef6f2d9d06fb8984d0c317ba4ad484036f
raseccaos <cesar.oros@edu.uah.es>
2026-04-30 09:50:51 +0200

    medio agregado las interfaces de parte 2

---
+++ PAvanz/src/main/java/PA/Computa2.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package PA;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+/**
+ *
+ * @author cesar
+ */
+public class Computa2 extends UnicastRemoteObject implements InterfaceHawkins{
+    
+}
---
+++ PAvanz/src/main/java/PA/InterfaceHawkins.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package PA;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+/**
+ *
+ * @author cesar
+ */
+public interface InterfaceHawkins {
+    
+}
